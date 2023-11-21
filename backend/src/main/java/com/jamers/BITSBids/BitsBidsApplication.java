97fac5e8dc962a63ed2e0e1268c0ef9971cbf98c
skoriop <karthikp.ivy@gmail.com>
2023-11-22 00:36:33 +0530

    feat: enable R2DBC repositories

--- backend/src/main/java/com/jamers/BITSBids/BitsBidsApplication.java
+++ backend/src/main/java/com/jamers/BITSBids/BitsBidsApplication.java
@@ -2,8 +2,10 @@ package com.jamers.BITSBids;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 public class BitsBidsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BitsBidsApplication.class, args);
