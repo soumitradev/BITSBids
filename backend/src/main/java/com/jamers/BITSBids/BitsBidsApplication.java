ee7bb319f31af37097d09fb255041790e0fbadf6
soumitradev <soumitra.shewale.dev@gmail.com>
2023-11-21 19:06:16 +0530

    feat: enable async on SpringBootApplication

--- backend/src/main/java/com/jamers/BITSBids/BitsBidsApplication.java
+++ backend/src/main/java/com/jamers/BITSBids/BitsBidsApplication.java
@@ -2,8 +2,10 @@ package com.jamers.BITSBids;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class BitsBidsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BitsBidsApplication.class, args);
