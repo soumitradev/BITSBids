6c0b23597f58041d413d25e15f3c9a8fdef550d2
soumitradev <soumitra.shewale.dev@gmail.com>
2023-11-25 20:21:05 +0530

    feat: add minimum bid delta constant

--- backend/src/main/java/com/jamers/BITSBids/common/Constants.java
+++ backend/src/main/java/com/jamers/BITSBids/common/Constants.java
@@ -3,4 +3,6 @@ package com.jamers.BITSBids.common;
 public class Constants {
 	public static final float INITIAL_BALANCE = 200f;
 	public static final int MAX_MEDIA_SIZE = 8;
+	public static final int MIN_BID_DELTA = 1;
+	//	TODO: Write implementation for this
 }
