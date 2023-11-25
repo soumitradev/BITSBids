efe211ddac88ce2e9c2841686900358cf4a9f238
soumitradev <soumitra.shewale.dev@gmail.com>
2023-11-25 20:23:21 +0530

    refactor: add NotBlack constraints to UserCreateData

--- backend/src/main/java/com/jamers/BITSBids/request_models/UserCreateData.java
+++ backend/src/main/java/com/jamers/BITSBids/request_models/UserCreateData.java
@@ -1,11 +1,13 @@
 package com.jamers.BITSBids.request_models;
 
 import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.math.BigInteger;
 
-public record UserCreateData(@NotEmpty String room, @Digits(
+public record UserCreateData(@NotEmpty @NotBlank String room, @Digits(
 				integer = 10,
 				fraction = 0
 ) BigInteger phoneNumber) {
+//	TODO: Check null phone number
 }
