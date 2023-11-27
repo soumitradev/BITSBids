b4481e355de5ce0096205c4e774be8a313050ca1
SaitejSamudrala <f20220241@hyderabad.bits-pilani.ac.in>
2023-11-27 16:35:39 +0530

    feat: created user/me , user/delete and user/edit  endpoints

--- backend/src/main/java/com/jamers/BITSBids/controllers/UserController.java
+++ backend/src/main/java/com/jamers/BITSBids/controllers/UserController.java
@@ -3,8 +3,11 @@ package com.jamers.BITSBids.controllers;
 import com.jamers.BITSBids.models.User;
 import com.jamers.BITSBids.repositories.UserRepository;
 import com.jamers.BITSBids.request_models.UserCreateData;
+import com.jamers.BITSBids.request_models.UserEditData;
 import com.jamers.BITSBids.response_types.GenericResponseType;
 import com.jamers.BITSBids.response_types.errors.UserCreateError;
+import com.jamers.BITSBids.response_types.errors.AuthUserError;
+import com.jamers.BITSBids.response_types.errors.UserEditError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +16,15 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import java.math.BigInteger;
 
 import static com.jamers.BITSBids.common.Constants.INITIAL_BALANCE;
 
@@ -33,60 +39,60 @@ public class UserController {
 	}
 
 	@PostMapping(
-					path = "/api/user/create",
-					consumes = MediaType.APPLICATION_JSON_VALUE,
-					produces = MediaType.APPLICATION_JSON_VALUE
+			path = "/api/user/create",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE
 	)
 	public ResponseEntity<GenericResponseType> create(
-					@AuthenticationPrincipal
-					OAuth2User principal,
-					@RequestBody
-					@Validated
-					UserCreateData userCreateData) {
+			@AuthenticationPrincipal
+			OAuth2User principal,
+			@RequestBody
+			@Validated
+			UserCreateData userCreateData) {
 		if (userCreateData == null) {
 			return new ResponseEntity<GenericResponseType>(
-							new GenericResponseType(
-											UserCreateError.nullUserError(),
-											GenericResponseType.ResponseStatus.ERROR
-							),
-							HttpStatus.BAD_REQUEST
+					new GenericResponseType(
+							UserCreateError.nullUserError(),
+							GenericResponseType.ResponseStatus.ERROR
+					),
+					HttpStatus.BAD_REQUEST
 			);
 		}
 		if (principal.getAttribute("email") == null || Objects.requireNonNull(principal.getAttribute("email")).toString().isEmpty() || Objects.requireNonNull(
-						principal.getAttribute("email")).toString().isBlank()) {
+				principal.getAttribute("email")).toString().isBlank()) {
 			return new ResponseEntity<GenericResponseType>(
-							new GenericResponseType(
-											UserCreateError.nullEmailError(),
-											GenericResponseType.ResponseStatus.ERROR
-							),
-							HttpStatus.BAD_REQUEST
+					new GenericResponseType(
+							UserCreateError.nullEmailError(),
+							GenericResponseType.ResponseStatus.ERROR
+					),
+					HttpStatus.BAD_REQUEST
 			);
 		}
 
 		if (principal.getAttribute("name") == null || Objects.requireNonNull(principal.getAttribute("name")).toString().isEmpty() || Objects.requireNonNull(
-						principal.getAttribute("name")).toString().isBlank()) {
+				principal.getAttribute("name")).toString().isBlank()) {
 			return new ResponseEntity<GenericResponseType>(
-							new GenericResponseType(
-											UserCreateError.nullNameError(),
-											GenericResponseType.ResponseStatus.ERROR
-							),
-							HttpStatus.BAD_REQUEST
+					new GenericResponseType(
+							UserCreateError.nullNameError(),
+							GenericResponseType.ResponseStatus.ERROR
+					),
+					HttpStatus.BAD_REQUEST
 			);
 		}
 
 		if (userRepository.findByEmail(Objects.requireNonNull(principal.getAttribute("email")).toString()).blockFirst() != null) {
 			return new ResponseEntity<GenericResponseType>(
-							new GenericResponseType(
-											UserCreateError.userAlreadyExistsError(),
-											GenericResponseType.ResponseStatus.ERROR
-							),
-							HttpStatus.BAD_REQUEST
+					new GenericResponseType(
+							UserCreateError.userAlreadyExistsError(),
+							GenericResponseType.ResponseStatus.ERROR
+					),
+					HttpStatus.BAD_REQUEST
 			);
 		}
 
 		Pattern pattern = Pattern.compile(
-						"f(\\d{4})\\d{4}@(hyderabad|goa|pilani).bits-pilani.ac.in",
-						Pattern.CASE_INSENSITIVE
+				"f(\\d{4})\\d{4}@(hyderabad|goa|pilani).bits-pilani.ac.in",
+				Pattern.CASE_INSENSITIVE
 		);
 		Matcher matcher = pattern.matcher(Objects.requireNonNull(principal.getAttribute("email")));
 		if (matcher.find()) {
@@ -95,34 +101,142 @@ public class UserController {
 				batch = Integer.parseInt(matcher.group(1));
 			} catch (NumberFormatException e) {
 				return new ResponseEntity<GenericResponseType>(
-								new GenericResponseType(
-												UserCreateError.invalidEmailError(),
-												GenericResponseType.ResponseStatus.ERROR
-								),
-								HttpStatus.UNAUTHORIZED
+						new GenericResponseType(
+								UserCreateError.invalidEmailError(),
+								GenericResponseType.ResponseStatus.ERROR
+						),
+						HttpStatus.UNAUTHORIZED
 				);
 			}
 			User user = new User(
-							null,
-							Objects.requireNonNull(principal.getAttribute("email")).toString(),
-							Objects.requireNonNull(principal.getAttribute("name")).toString(),
-							batch,
-							userCreateData.room(),
-							userCreateData.phoneNumber(),
-							INITIAL_BALANCE
+					null,
+					Objects.requireNonNull(principal.getAttribute("email")).toString(),
+					Objects.requireNonNull(principal.getAttribute("name")).toString(),
+					batch,
+					userCreateData.room(),
+					userCreateData.phoneNumber(),
+					INITIAL_BALANCE
 			);
 			return new ResponseEntity<GenericResponseType>(new GenericResponseType(
-							userRepository.save(user).block(),
-							GenericResponseType.ResponseStatus.SUCCESS
+					userRepository.save(user).block(),
+					GenericResponseType.ResponseStatus.SUCCESS
 			), HttpStatus.CREATED);
 		} else {
 			return new ResponseEntity<GenericResponseType>(
-							new GenericResponseType(
-											UserCreateError.invalidEmailError(),
-											GenericResponseType.ResponseStatus.ERROR
-							),
-							HttpStatus.UNAUTHORIZED
+					new GenericResponseType(
+							UserCreateError.invalidEmailError(),
+							GenericResponseType.ResponseStatus.ERROR
+					),
+					HttpStatus.UNAUTHORIZED
 			);
 		}
 	}
+
+	@GetMapping("/api/user/me")
+	public ResponseEntity<GenericResponseType> getMe(@AuthenticationPrincipal OAuth2User principal) {
+		final User currentUser = userRepository.findByEmail(Objects.requireNonNull(principal.getAttribute("email")).toString()).blockFirst();
+
+		if (currentUser == null) {
+			return new ResponseEntity<GenericResponseType>(new GenericResponseType(
+					AuthUserError.nullUserError(),
+					GenericResponseType.ResponseStatus.ERROR
+			), HttpStatus.BAD_REQUEST);
+		} else {
+			return new ResponseEntity<GenericResponseType>(new GenericResponseType(
+					currentUser,
+					GenericResponseType.ResponseStatus.SUCCESS
+			), HttpStatus.ACCEPTED);
+		}
+
+	}
+
+	@PostMapping(
+			path = "/api/user/delete",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE
+
+	)
+	public ResponseEntity<GenericResponseType> delete(
+			@AuthenticationPrincipal
+			OAuth2User principal
+	) {
+		final User currentUser = userRepository.findByEmail(Objects.requireNonNull(principal.getAttribute("email")).toString()).blockFirst();
+		if (currentUser == null) {
+			return new ResponseEntity<GenericResponseType>(new GenericResponseType(
+					AuthUserError.nullUserError(),
+					GenericResponseType.ResponseStatus.ERROR
+			), HttpStatus.BAD_REQUEST);
+		} else {
+			return new ResponseEntity<GenericResponseType>(new GenericResponseType(
+					userRepository.delete(currentUser).block(),
+					GenericResponseType.ResponseStatus.SUCCESS
+			), HttpStatus.ACCEPTED);
+		}
+
+	}
+
+	@PostMapping(
+			path = "/api/user/edit",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE
+
+	)
+	public ResponseEntity<GenericResponseType> edit(
+			@AuthenticationPrincipal
+			OAuth2User principal,
+			@RequestBody
+			@Validated
+			UserEditData userEditData) {
+		final User currentUser = userRepository.findByEmail(Objects.requireNonNull(principal.getAttribute("email")).toString()).blockFirst();
+		if (currentUser == null) {
+			return new ResponseEntity<GenericResponseType>(new GenericResponseType(
+					AuthUserError.nullUserError(),
+					GenericResponseType.ResponseStatus.ERROR
+			), HttpStatus.BAD_REQUEST);
+		}
+		if (userEditData == null) {
+			return new ResponseEntity<GenericResponseType>(
+					new GenericResponseType(
+							UserEditError.nullFieldsError(),
+							GenericResponseType.ResponseStatus.ERROR
+					),
+					HttpStatus.BAD_REQUEST
+			);
+		}
+
+		String newRoom = currentUser.room();
+		BigInteger newPhoneNumber = currentUser.phoneNumber();
+
+		if (userEditData.room() != null) {
+			newRoom = userEditData.room();
+
+
+		}
+		if (userEditData.phoneNumber() != null) {
+			newPhoneNumber = userEditData.phoneNumber();
+
+		}
+
+
+		User user = new User(
+				currentUser.id(),
+				currentUser.email(),
+				currentUser.name(),
+				currentUser.batch(),
+				newRoom,
+				newPhoneNumber,
+				currentUser.balance()
+
+		);
+
+		return new ResponseEntity<GenericResponseType>(new GenericResponseType(
+				userRepository.save(user).block(),
+				GenericResponseType.ResponseStatus.SUCCESS
+		), HttpStatus.ACCEPTED);
+
+
+	}
+
+
 }
+
---
+++ backend/src/main/java/com/jamers/BITSBids/request_models/UserEditData.java
@@ -0,0 +1,12 @@
+package com.jamers.BITSBids.request_models;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import java.math.BigInteger;
+
+
+public record UserEditData(@NotBlank String room, @Digits(
+		integer = 10,
+		fraction = 0
+) BigInteger phoneNumber) {
+}
