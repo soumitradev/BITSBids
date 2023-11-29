bc3ff7742f0842a485ae2e6da112a81d76c79358
soumitradev <soumitra.shewale.dev@gmail.com>
2023-11-29 07:25:45 +0530

    feat: define seaweedfs utility classes

---
+++ backend/src/main/java/com/jamers/BITSBids/common/SeaweedFile.java
@@ -0,0 +1,11 @@
+package com.jamers.BITSBids.common;
+
+public class SeaweedFile {
+	public int size;
+	public String name;
+	public String eTag;
+
+	public String toString() {
+		return "SeaweedFile(size=" + this.size + ", name=" + this.name + ", eTag=" + this.eTag + ")";
+	}
+}
