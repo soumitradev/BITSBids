bf617dade64bd818cc46e4ac3d4f73ee456e2e58
skoriop <karthikp.ivy@gmail.com>
2023-11-29 17:44:41 +0530

    fix: store session based on email instead of principal ID

--- backend/src/main/java/com/jamers/BITSBids/config/ChatWebSocketHandler.java
+++ backend/src/main/java/com/jamers/BITSBids/config/ChatWebSocketHandler.java
@@ -4,12 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.security.Principal;
 import java.util.Map;
 
 import static org.springframework.web.socket.CloseStatus.SERVER_ERROR;
@@ -22,14 +22,16 @@ class ChatWebSocketHandler extends TextWebSocketHandler {
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		Principal principal = session.getPrincipal();
+		OAuth2AuthenticationToken principal = ((OAuth2AuthenticationToken) session.getPrincipal());
 		if (principal == null || principal.getName() == null) {
 			session.close(SERVER_ERROR.withReason("Unauthorised"));
 			return;
 		}
 
-		sessions.put(principal.getName(), session);
-		logger.info("Server connection {} from principal {} opened", session.getId(), principal.getName());
+		// TODO: think about multiple sessions per user
+		String email = principal.getPrincipal().getAttribute("email");
+		sessions.put(email, session);
+		logger.info("Server connection {} from principal {} opened", session.getId(), principal.getPrincipal().getAttribute("email"));
 
 		TextMessage message = new TextMessage(String.format("Connected to server from principal %s with id %s", principal.getName(), session.getId()));
 		logger.info("Server sends: {}", message);
@@ -37,16 +39,21 @@ class ChatWebSocketHandler extends TextWebSocketHandler {
 	}
 
 	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message) {
+		logger.info("Server received: {}", message);
+	}
+
+	@Override
 	public void afterConnectionClosed(WebSocketSession session,
 	                                  @NotNull
 	                                  CloseStatus status) {
-		Principal principal = session.getPrincipal();
+		OAuth2AuthenticationToken principal = ((OAuth2AuthenticationToken) session.getPrincipal());
 		if (principal == null || principal.getName() == null) {
 			return;
 		}
-
+		String email = principal.getPrincipal().getAttribute("email");
 		logger.info("Server connection {} from principal {} closed with status {}", session.getId(), principal.getName(), status);
-		sessions.remove(principal.getName());
+		sessions.remove(email);
 	}
 
 	@Override
