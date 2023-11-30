629401181a63cc33c69286d57b4fb38f1069ef83
skoriop <karthikp.ivy@gmail.com>
2023-11-30 20:26:06 +0000

    fix: fix colours

--- frontend/src/components/ConversationBubble.tsx
+++ frontend/src/components/ConversationBubble.tsx
@@ -7,7 +7,7 @@ const ConversationBubble = (props: {
   unreadCount: number;
 }) => {
   return (
-    <div class="flex flex-row justify-between items-center gap-2 p-2 border-t bg-slate-900 hover:bg-slate-600">
+    <div class="flex flex-row justify-between items-center gap-2 p-2 border-t bg-slate-900 hover:bg-slate-700">
       <div class="flex flex-col justify-self-start flex-grow mb-0.5">
         <Label class="text-lg font-medium line-clamp-1 max-w-[75%]">
           {props.title}
@@ -17,7 +17,7 @@ const ConversationBubble = (props: {
         </Label>
       </div>
       {props.unreadCount > 0 && (
-        <Badge class="border-slate-400 rounded-full">
+        <Badge class="bg-slate-300 rounded-full py-1">
           <Label class="font-semibold">{props.unreadCount}</Label>
         </Badge>
       )}
--- frontend/src/components/MessageBubble.tsx
+++ frontend/src/components/MessageBubble.tsx
@@ -10,10 +10,12 @@ const MessageBubble = (props: {
     <div
       class={cn(
         "w-fit max-w-[55%] flex flex-col items-center py-1 px-2 border-t gap-0.5 rounded-lg",
-        props.fromSelf ? "self-end bg-blue-600" : "self-start bg-slate-600"
+        props.fromSelf ? "self-end bg-blue-700" : "self-start bg-slate-600"
       )}
     >
-      <Label class="self-start leading-tight">{props.text}</Label>
+      <Label class="self-start leading-tight text-slate-200">
+        {props.text}
+      </Label>
       <Label class="text-xs text-slate-400 self-end">{props.timestamp}</Label>
     </div>
   );
