738fb7ced8cbee3c7d58c9fe10b24c2ef79317e2
skoriop <karthikp.ivy@gmail.com>
2023-11-30 16:05:19 +0000

    feat: add props to components

--- frontend/src/components/ConversationBubble.tsx
+++ frontend/src/components/ConversationBubble.tsx
@@ -2,7 +2,11 @@ import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Label } from "./ui/label";
 import { Badge } from "./ui/badge";
 
-const ConversationBubble = () => {
+const ConversationBubble = (props: {
+  title: string;
+  latestMessage: string;
+  unreadCount: number;
+}) => {
   return (
     <div class="flex flex-row justify-between items-center gap-2 p-2 border-t bg-slate-900 hover:bg-slate-600">
       <Avatar class="mr-1">
@@ -10,12 +14,14 @@ const ConversationBubble = () => {
         <AvatarFallback>EK</AvatarFallback>
       </Avatar>
       <div class="flex flex-col justify-self-start flex-grow mb-0.5">
-        <Label class="text-lg font-medium">Jaming Set</Label>
-        <Label class="text-xs text-slate-400">You: ill pop you ra</Label>
+        <Label class="text-lg font-medium">{props.title}</Label>
+        <Label class="text-xs text-slate-400">{props.latestMessage}</Label>
       </div>
-      <Badge class="border-slate-400 rounded-full">
-        <Label class="font-semibold">2</Label>
-      </Badge>
+      {props.unreadCount > 0 && (
+        <Badge class="border-slate-400 rounded-full">
+          <Label class="font-semibold">{props.unreadCount}</Label>
+        </Badge>
+      )}
     </div>
   );
 };
--- frontend/src/components/MessageBubble.tsx
+++ frontend/src/components/MessageBubble.tsx
@@ -1,12 +1,20 @@
-import { Badge } from "./ui/badge";
-import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
 import { Label } from "./ui/label";
+import { cn } from "~/lib/utils";
 
-const MessageBubble = () => {
+const MessageBubble = (props: {
+  text: string;
+  timestamp: string;
+  fromSelf: boolean;
+}) => {
   return (
-    <div class="w-fit max-w-[60%] flex flex-col items-center py-1 px-2 border-t gap-0.5 bg-blue-600 rounded-lg">
-      <Label class="self-start leading-tight">i very much miss cc someone please kill me please</Label>
-      <Label class="text-xs text-slate-400 self-end">6:09 pm</Label>
+    <div
+      class={cn(
+        "w-fit max-w-[55%] flex flex-col items-center py-1 px-2 border-t gap-0.5 rounded-lg",
+        props.fromSelf ? "self-end bg-blue-600" : "self-start bg-slate-600"
+      )}
+    >
+      <Label class="self-start leading-tight">{props.text}</Label>
+      <Label class="text-xs text-slate-400 self-end">{props.timestamp}</Label>
     </div>
   );
 };
