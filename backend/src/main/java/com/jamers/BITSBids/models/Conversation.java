3977cb4d138475683a3395fbeb9f1a9eb4982b0d
skoriop <karthikp.ivy@gmail.com>
2023-11-30 10:22:49 +0530

    fix: updated Conversation record to match schema

--- backend/src/main/java/com/jamers/BITSBids/models/Conversation.java
+++ backend/src/main/java/com/jamers/BITSBids/models/Conversation.java
@@ -4,6 +4,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("conversations")
-public record Conversation(@Id Integer id, int sellerId, int buyerId, Integer lastReadBySellerId,
+public record Conversation(@Id Integer id, int sellerId, int buyerId, int productId, Integer lastReadBySellerId,
                            Integer lastReadByBuyerId) {
 }
