7ab546b264eb939d5f7fc621a5cbbf8bf2f6dbe3
Anirudh-idk <aniragg30@gmail.com>
2023-12-01 12:36:27 +0530

    fix: fix inactive bids query

--- backend/src/main/java/com/jamers/BITSBids/repositories/BidRepository.java
+++ backend/src/main/java/com/jamers/BITSBids/repositories/BidRepository.java
@@ -20,6 +20,6 @@ public interface BidRepository extends ReactiveCrudRepository<Bid, String>, Reac
 	@Query("SELECT product.* FROM bitsbids.bids AS bid INNER JOIN bitsbids.products AS product ON bid.bidder_id = :id AND product.sold = true AND bid.product_id = product.id INNER JOIN (SELECT bid.product_id, MAX(bid.price) AS maxPrice FROM bitsbids.bids AS bid GROUP BY bid.product_id) AS maxBid ON bid.product_id = maxBid.product_id AND bid.price = maxBid.maxPrice")
 	Flux<Product> findInactiveBidProductsByUserId(int id);
 
-	@Query("SELECT product.* FROM bitsbids.bids AS bid INNER JOIN bitsbids.products AS product ON bid.bidder_id = :id AND product.sold = true AND bid.product_id = product.id INNER JOIN (SELECT bid.product_id, MAX(bid.price) AS maxPrice FROM bitsbids.bids AS bid GROUP BY bid.product_id) AS maxBid ON bid.product_id = maxBid.product_id AND bid.price = maxBid.maxPrice")
+	@Query("SELECT bid.* FROM bitsbids.bids AS bid INNER JOIN bitsbids.products AS product ON bid.bidder_id = :id AND product.sold = true AND bid.product_id = product.id INNER JOIN (SELECT bid.product_id, MAX(bid.price) AS maxPrice FROM bitsbids.bids AS bid GROUP BY bid.product_id) AS maxBid ON bid.product_id = maxBid.product_id AND bid.price = maxBid.maxPrice")
 	Flux<Bid> findInactiveBidsByUserId(int id);
 }
