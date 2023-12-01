46489b2d054be9c56a086367dc069e3c2c6c0621
soumitradev <soumitra.shewale.dev@gmail.com>
2023-12-01 07:24:00 +0530

    fix: tweak product card to look bettter

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,46 +1,77 @@
-import {Progress} from "./ui/progress.tsx"
-import {createSignal} from "solid-js";
-import {A} from "@solidjs/router";
+import { Progress } from "./ui/progress.tsx";
+import { createSignal } from "solid-js";
+import { A } from "@solidjs/router";
 
-export default function ProductCard(props: {
-    id: number;
-    imgUrl: string;
-    name: string;
-    price: number;
-    createdAt: Date;
-    closedAt: Date
+export default function ProductCard({
+  id,
+  imgUrl,
+  name,
+  price,
+  createdAt,
+  closedAt,
+}: {
+  id: number;
+  imgUrl: string;
+  name: string;
+  price: number;
+  createdAt: Date;
+  closedAt: Date;
 }) {
-    const CLOSE = props.closedAt;
-    const START = props.createdAt;
-    const TOTAL_TIME = CLOSE.getTime() - START.getTime();
-    let [current_diff, setCurrentDiff] = createSignal(CLOSE.getTime() - Date.now());
-    let [outputDiff, setOutputDiff] = createSignal("");
-    const handleDiff = () => {
-        setCurrentDiff(current_diff() - 1000);
-        let output_diff: string = ""
-        if (Math.floor(current_diff() / 3600000) > 0) {
-            output_diff = `Ends in ${Math.floor(current_diff() / 3600000)}h ${Math.floor(current_diff() % 3600000 / 60000)}min`;
-        } else if (Math.floor((current_diff() % 3600000) / 60000) > 0) {
-            output_diff = `Ends in ${Math.floor(current_diff() % 3600000 / 60000)}min`;
-        } else if (Math.floor((current_diff() % (3600000 * 60000)) / 1000) > 0) {
-            output_diff = `Ends in ${Math.floor(current_diff() % (3600000 * 60000) / 1000)}sec`
-        } else {
-            output_diff = "Auction Closed"
-        }
-        setOutputDiff(output_diff)
-    }
-    setInterval(handleDiff, 1000)
-    return (
-        <A class="flex flex-col w-48 h-72 rounded-lg border-2 gap-1 align-left" href={`/products/${props.id}`}>
-            <img class="p-0 m-0 h-2/3 rounded-lg  object-fill" src={props.imgUrl} alt="smtg"/>
-            <div class="p-2">
-                <h3>{props.name}</h3>
-                <Progress value={100 - (current_diff() * 100 / TOTAL_TIME)}/>
-                <div class="flex gap-1 pb-3">
+  let [currentDiff, setCurrentDiff] = createSignal(
+    closedAt.getTime() - Date.now()
+  );
+  let [outputDiff, setOutputDiff] = createSignal("");
 
-                    <p><strong>R{props.price}</strong> {outputDiff()}</p>
-                </div>
-            </div>
-        </A>
-    )
+  const handleDiff = () => {
+    setCurrentDiff(currentDiff() - 1000);
+    let output_diff: string = "";
+    if (Math.floor(currentDiff() / 3600000) > 0) {
+      output_diff = `Ends in ${Math.floor(
+        currentDiff() / 3600000
+      )}h ${Math.floor((currentDiff() % 3600000) / 60000)}min`;
+    } else if (Math.floor((currentDiff() % 3600000) / 60000) > 0) {
+      output_diff = `Ends in ${Math.floor(
+        (currentDiff() % 3600000) / 60000
+      )}min`;
+    } else if (Math.floor((currentDiff() % (3600000 * 60000)) / 1000) > 0) {
+      output_diff = `Ends in ${Math.floor(
+        (currentDiff() % (3600000 * 60000)) / 1000
+      )}sec`;
+    } else {
+      output_diff = "Auction Closed";
+    }
+    setOutputDiff(output_diff);
+  };
+
+  setInterval(handleDiff, 1000);
+
+  return (
+    <A
+      class="flex flex-col w-64 h-80 rounded-lg border-2 gap-2 align-left"
+      href={`/products/${id}`}
+    >
+      <img
+        class="p-0 m-0 h-2/3 rounded-lg object-cover"
+        src={imgUrl}
+        alt="smtg"
+      />
+      <div class="p-2">
+        <span class="font-bold">{name}</span>
+        <Progress
+          value={
+            currentDiff() < 0
+              ? 100
+              : 100 -
+                (currentDiff() * 100) /
+                  (closedAt.getTime() - createdAt.getTime())
+          }
+          class="my-1 mb-2"
+        />
+        <div class="flex gap-1 justify-between">
+          <span class="font-bold">R{price}</span>
+          <span class="text-slate-400">{outputDiff()}</span>
+        </div>
+      </div>
+    </A>
+  );
 }
