7a4f97dfa969d1fc4bb06bf764ec319d2b6504cc
skoriop <karthikp.ivy@gmail.com>
2023-12-01 10:25:41 +0530

    feat: update models

--- backend/src/main/java/com/jamers/BITSBids/response_models/CategorizedProduct.java
+++ backend/src/main/java/com/jamers/BITSBids/response_models/CategorizedProduct.java
@@ -1,9 +1,10 @@
 package com.jamers.BITSBids.response_models;
 
 import com.jamers.BITSBids.models.Bid;
+import com.jamers.BITSBids.models.Category;
 import com.jamers.BITSBids.models.Product;
 
 import java.util.List;
 
-public record CategorizedProduct(Product product, List<String> Categories, List<Bid> Bids) {
+public record CategorizedProduct(Product product, List<Category> Categories, List<Bid> Bids) {
 }
