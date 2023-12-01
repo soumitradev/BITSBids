f0806063d2cf0b32bf11a087d4811099e4810967
soumitradev <soumitra.shewale.dev@gmail.com>
2023-12-01 07:23:34 +0530

    fix: change images to be object-cover

--- frontend/src/components/CategoryCard.tsx
+++ frontend/src/components/CategoryCard.tsx
@@ -1,11 +1,21 @@
-import {A} from "@solidjs/router";
+import { A } from "@solidjs/router";
 
-export default function CategoryCard(props: { id: number; imgUrl: string; name: string }) {
-
-    return (
-        <A class="flex flex-col w-56 h-60 rounded-lg border-2 gap-1 align-middle" href={`/categoriee/${props.id}`}>
-            <img class="p-0 m-0 h-4/5 rounded-lg object-fill" src={props.imgUrl} alt="Category_image"/>
-            <p class="font-bold text-xl self-center">{props.name}</p>
-        </A>
-    )
+export default function CategoryCard(props: {
+  id: number;
+  imgUrl: string;
+  name: string;
+}) {
+  return (
+    <A
+      class="flex flex-col w-56 h-60 rounded-lg border-2 gap-1 align-middle"
+      href={`/categoriee/${props.id}`}
+    >
+      <img
+        class="p-0 m-0 h-4/5 rounded-lg object-cover"
+        src={props.imgUrl}
+        alt="Category_image"
+      />
+      <p class="font-bold text-xl self-center">{props.name}</p>
+    </A>
+  );
 }
